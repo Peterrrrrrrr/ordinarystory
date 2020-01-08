393eb044f15f79f0380746691f6085e3703d3a8e
huhu <1052833858@qq.com>
2020-01-09 07:17:30 +0800

    add createComponents

---
+++ src/components/Header/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <header id="header">
+    <div class="content">
+      <ul>
+        <li>
+          <p>huhuLife</p>
+        </li>
+        <li>
+          <p>|</p>
+        </li>
+        <li class="backpack">
+          <i class="icon iconfont icon-shubao"></i>
+        </li>
+        <li class="mac">
+          <i class="icon iconfont icon-diannao"></i>
+        </li>
+        <li class="coffe">
+          <i class="icon iconfont icon-iconkafei"></i>
+        </li>
+        <li >
+          <i class="icon iconfont icon-jita-liangse-"></i>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
+
+<style>
+#header {
+  width: 100%;
+  height: 49px;
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-bottom: 1px solid rgb(238, 238, 238);
+}
+#header .content {
+  display: flex;
+  height: 49px;
+  align-items: center;
+  justify-content: center;
+}
+#header ul {
+  display: flex;
+  text-align: center;
+  height: 49px;
+  width: 60%;
+  align-items: center;
+}
+#header ul li {
+  flex: 1;
+  height: 40px;
+  padding: 0;
+}
+#header ul .backpack i {
+  font-size: 18px;
+}
+#header ul .mac{
+  margin-top: 2px;
+}
+#header ul .coffe{
+  margin-bottom: 5px;
+}
+#header ul i {
+  font-size: 20px;
+  color: #666666;
+}
+#header ul p {
+  font-size: 18px;
+  font-family:'微軟正黑體';
+  color: #666666;
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <div>
+    <Header />
+    <!-- <div class="begin">
+      <i class="icon iconfont icon-shang-copy-copy"></i>
+      <i class="icon iconfont icon-shang-copy-copy"></i>
+      <i class="icon iconfont icon-shang-copy-copy"></i>
+    </div> -->
+    <div>
+      <ul class="wrap">
+        <li class="begin">
+          <i class="icon iconfont icon-shang-copy-copy"></i>
+          <i class="icon iconfont icon-shang-copy-copy"></i>
+          <i class="icon iconfont icon-shang-copy-copy"></i>
+        </li>
+        <li>
+          <img src="/img/1.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/2.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/3.jpg" alt />
+        </li>
+        <li>
+          <img src="/img/4.jpg" alt />
+        </li>
+        <li>
+          <img src="/img/5.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/6.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/7.jpg" alt class="test" />
+        </li>
+        <li>
+          <img src="/img/8.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/9.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/10.jpg" alt />
+        </li>
+        <li>
+          <img src="/img/11.jpg" alt />
+        </li>
+        <li>
+          <img src="/img/12.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/13.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/14.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/15.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/16.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/18.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/19.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/20.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/21.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/22.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/23.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/24.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/25.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/26.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/27.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/28.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/29.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/30.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/31.JPG" alt />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+export default {
+  name: "home",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+.begin {
+  margin-top: 300px;
+  margin-bottom: 150px;
+}
+.begin i {
+  font-size: 50px;
+  color: #333333;
+}
+.wrap {
+  padding-left: 0;
+}
+li {
+  margin: 0 auto;
+  padding-bottom: 200px;
+  display: flex;
+  width: 80%;
+  /* height: 80%; */
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 90%;
+}
+.test {
+  width: 70%;
+}
+</style>
