e1623646fa5080cbceda2b7bf823f168610beb4e
huhu <1052833858@qq.com>
2020-01-10 20:23:54 +0800

    bug clear

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <Header />
-    <!-- <div class="begin">
-      <i class="icon iconfont icon-shang-copy-copy"></i>
-      <i class="icon iconfont icon-shang-copy-copy"></i>
-      <i class="icon iconfont icon-shang-copy-copy"></i>
-    </div> -->
     <div>
       <ul class="wrap">
         <li class="begin">
@@ -26,19 +21,28 @@
           <img src="/img/4.jpg" alt />
         </li>
         <li>
+          <img class="square" src="/img/4a.jpg" alt />
+        </li>
+        <li>
+          <img src="/img/4b.jpg" alt />
+        </li>
+        <li>
           <img src="/img/5.JPG" alt />
         </li>
         <li>
-          <img src="/img/6.JPG" alt />
+          <img class="rectangle" src="/img/6b.JPG" alt />
         </li>
         <li>
-          <img src="/img/7.jpg" alt class="test" />
+          <img class="square" src="/img/6a.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/7.jpg" alt />
         </li>
         <li>
           <img src="/img/8.JPG" alt />
         </li>
         <li>
-          <img src="/img/9.JPG" alt />
+          <img class="square" src="/img/9.JPG" alt />
         </li>
         <li>
           <img src="/img/10.jpg" alt />
@@ -50,7 +54,7 @@
           <img src="/img/12.JPG" alt />
         </li>
         <li>
-          <img src="/img/13.JPG" alt />
+          <img class="square" src="/img/13.JPG" alt />
         </li>
         <li>
           <img src="/img/14.JPG" alt />
@@ -59,37 +63,40 @@
           <img src="/img/15.JPG" alt />
         </li>
         <li>
-          <img src="/img/16.JPG" alt />
+          <img class="square" src="/img/16.JPG" alt />
         </li>
         <li>
-          <img src="/img/18.JPG" alt />
+          <img class="square" src="/img/17.JPG" alt />
         </li>
         <li>
-          <img src="/img/19.JPG" alt />
+          <img class="square" src="/img/18.JPG" alt />
+        </li>
+        <li>
+          <img class="square" src="/img/19.JPG" alt />
         </li>
         <li>
           <img src="/img/20.JPG" alt />
         </li>
         <li>
-          <img src="/img/21.JPG" alt />
+          <img class="square" src="/img/21.JPG" alt />
         </li>
         <li>
-          <img src="/img/22.JPG" alt />
+          <img class="rectangle" src="/img/22.JPG" alt />
         </li>
         <li>
           <img src="/img/23.JPG" alt />
         </li>
         <li>
-          <img src="/img/24.JPG" alt />
+          <img class="rectangle" src="/img/24.JPG" alt />
         </li>
         <li>
-          <img src="/img/25.JPG" alt />
+          <img class="square" src="/img/25.JPG" alt />
         </li>
         <li>
           <img src="/img/26.JPG" alt />
         </li>
         <li>
-          <img src="/img/27.JPG" alt />
+          <img class="square" src="/img/27.JPG" alt />
         </li>
         <li>
           <img src="/img/28.JPG" alt />
@@ -101,19 +108,49 @@
           <img src="/img/30.JPG" alt />
         </li>
         <li>
-          <img src="/img/31.JPG" alt />
+          <img class="square" src="/img/31.JPG" alt />
+        </li>
+        <li>
+          <img class="square" src="/img/32.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/34.JPG" alt />
+        </li>
+        <li>
+          <img class="square" src="/img/34a.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/35.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/36.JPG" alt />
+        </li>
+        <li>
+          <img class="rectangle" src="/img/37.JPG" alt />
+        </li>
+        <li>
+          <img  class="square" src="/img/37a.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/38.JPG" alt />
+        </li>
+        <li>
+          <img src="/img/39.JPG" alt />
         </li>
       </ul>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
   name: "home",
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
@@ -143,7 +180,10 @@ li {
 img {
   width: 90%;
 }
-.test {
+.square {
+  width: 80%;
+}
+.rectangle {
   width: 70%;
 }
 </style>
